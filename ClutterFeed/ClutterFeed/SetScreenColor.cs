73992840ee4f916daf2732e2d759a71d35eb1fa6
Emile <emil44d8@edu.eal.dk>
2015-10-25 23:05:39 +0100

    Added GPL headers to all .cs files

--- ClutterFeed/ClutterFeed/SetScreenColor.cs
+++ ClutterFeed/ClutterFeed/SetScreenColor.cs
@@ -1,5 +1,21 @@
 ﻿// Thanks to Alex Shvedov for providing this file.
 
+/*   This file is part of ClutterFeed.
+ *
+ *    ClutterFeed is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation, either version 3 of the License, or
+ *    (at your option) any later version.
+ *
+ *    ClutterFeed is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with ClutterFeed. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 using System;
 using System.Diagnostics;                // for Debug
 using System.Drawing;                    // for Color (add reference to  System.Drawing assembly)
