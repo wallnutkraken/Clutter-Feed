1f8d2bfe456fb00a700fe458d470fbdf7475b77a
Emile <emil44d8@edu.eal.dk>
2015-11-20 13:17:14 +0100

    cleanup

--- ClutterFeed/ClutterFeed/StatusCommunication.cs
+++ ClutterFeed/ClutterFeed/StatusCommunication.cs
@@ -13,12 +13,6 @@
  *    You should have received a copy of the GNU General Public License
  *    along with ClutterFeed. If not, see <http://www.gnu.org/licenses/>.
  */
-
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using TweetSharp;
 
 namespace ClutterFeed
