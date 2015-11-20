c2ecfe0d23a5b7a52302db38a1f6f142a54b4e97
Emile <emil44d8@edu.eal.dk>
2015-11-20 12:29:47 +0100

    Added an easy way to control the timer

---
+++ ClutterFeed/ClutterFeed/TimerMan.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClutterFeed
+{
+    static class TimerMan
+    {
+        public static void Pause()
+        {
+            Program.UpdateTimer.Change(System.Threading.Timeout.Infinite, System.Threading.Timeout.Infinite);
+        }
+        public static void Resume()
+        {
+            Program.UpdateTimer.Change(0, 1000);
+        }
+    }
+}
