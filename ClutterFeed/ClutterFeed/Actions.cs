00d3627392b5a9e0551c43ad5e3626291604137a
Emile <emil44d8@edu.eal.dk>
2015-12-15 13:18:00 +0100

    removed unused usings

--- ClutterFeed/ClutterFeed/Actions.cs
+++ ClutterFeed/ClutterFeed/Actions.cs
@@ -15,9 +15,6 @@
  */
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using TweetSharp;
 using System.Threading;
 using System.Diagnostics;
